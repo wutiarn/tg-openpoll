3c8c06d903976b18d07e61c1f1f818c32066da21
Dmitry Romanov <me@wutiarn.ru>
2017-01-02 00:55:55 +0300

    Send vote confirmations

--- src/main/kotlin/ru/wutiarn/tg/openvote/handlers/CallbackQueryHandler.kt
+++ src/main/kotlin/ru/wutiarn/tg/openvote/handlers/CallbackQueryHandler.kt
@@ -2,13 +2,7 @@ package ru.wutiarn.tg.openvote.handlers
 
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.model.CallbackQuery
-import com.pengrad.telegrambot.model.InlineQuery
-import com.pengrad.telegrambot.model.Message
-import com.pengrad.telegrambot.model.request.InlineKeyboardButton
-import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup
-import com.pengrad.telegrambot.model.request.InlineQueryResultArticle
-import com.pengrad.telegrambot.request.AnswerInlineQuery
-import com.vdurmont.emoji.EmojiParser
+import com.pengrad.telegrambot.request.SendMessage
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -19,7 +13,11 @@ open class CallbackQueryHandler(val bot: TelegramBot) {
     val logger: Logger = LoggerFactory.getLogger(InlineQueryHandler::class.java)
 
     fun handleInlineCallback(callback: CallbackQuery) {
-        logger.info("Received callback: ${callback.data()}")
+        val userId = callback.from().id()
+        logger.info("Received callback: ${callback.data()} from $userId")
+
+        val sendResponse = bot.execute(SendMessage(userId, "Vote confirmed: ${callback.data()}"))
+        logger.info("Confirmation result: $sendResponse")
     }
 
 }
