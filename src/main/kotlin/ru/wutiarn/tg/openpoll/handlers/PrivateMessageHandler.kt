9725dae1aae5192c2e9fdb04e87f5493ccc8c80c
Dmitry Romanov <me@wutiarn.ru>
2017-01-03 11:29:37 +0300

    Help and about commands

--- src/main/kotlin/ru/wutiarn/tg/openpoll/handlers/PrivateMessageHandler.kt
+++ src/main/kotlin/ru/wutiarn/tg/openpoll/handlers/PrivateMessageHandler.kt
@@ -6,7 +6,9 @@ import com.pengrad.telegrambot.model.Message
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup
 import com.pengrad.telegrambot.model.request.InlineQueryResultArticle
+import com.pengrad.telegrambot.model.request.ParseMode
 import com.pengrad.telegrambot.request.AnswerInlineQuery
+import com.pengrad.telegrambot.request.SendMessage
 import com.vdurmont.emoji.EmojiParser
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -19,5 +21,17 @@ open class PrivateMessageHandler(val bot: TelegramBot) {
 
     fun handleMessage(msg: Message) {
         logger.info("Received message from ${msg.chat().id()}: ${msg.text()}")
+
+        val resp = when(msg.text()) {
+            "/help" -> "This bot can create simple open polls. Just @mention me in any chat and start typing your question"
+            "/about" -> "This bot was developed by Dmitry Romanov (@wutiarn) at the beginning of 2017. Source code is " +
+                    "available on [GitHub](https://github.com/wutiarn/tg-openpoll), if you'd like to see some feature - contributions are welcome"
+            else -> null
+        }
+
+        resp?.let {
+            bot.execute(SendMessage(msg.chat().id(), resp).parseMode(ParseMode.Markdown))
+        }
+
     }
 }
