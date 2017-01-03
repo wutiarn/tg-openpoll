e99f29b4ae81d83f533af56a22651865728edebe
Dmitry Romanov <me@wutiarn.ru>
2017-01-03 13:18:41 +0300

    Add multiple result articles

--- src/main/kotlin/ru/wutiarn/tg/openpoll/handlers/InlineQueryHandler.kt
+++ src/main/kotlin/ru/wutiarn/tg/openpoll/handlers/InlineQueryHandler.kt
@@ -6,6 +6,7 @@ import com.pengrad.telegrambot.model.request.InlineQueryResultArticle
 import com.pengrad.telegrambot.model.request.InputTextMessageContent
 import com.pengrad.telegrambot.model.request.ParseMode
 import com.pengrad.telegrambot.request.AnswerInlineQuery
+import com.vdurmont.emoji.EmojiParser
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.StringRedisTemplate
@@ -22,33 +23,52 @@ open class InlineQueryHandler(val bot: TelegramBot,
 
     fun handleQuery(query: InlineQuery) {
         val queryText = query.query()
-        val pollId = UUID.randomUUID().toString()
-        val variants = arrayOf(
-                ":thumbsup:",
-                ":neutral_face:",
-                ":thumbsdown:"
+
+        val resultArticlesContent = listOf(
+                ResultArticleContent("Start :thumbsup: / :thumbsdown: poll",
+                        arrayOf(":thumbsup:", ":thumbsdown:")),
+                ResultArticleContent("Start :thumbsup: / :neutral_face: / :thumbsdown: poll",
+                        arrayOf(":thumbsup:", ":neutral_face:", ":thumbsdown:")),
+                ResultArticleContent("Start :ok_hand: poll",
+                        arrayOf(":ok_hand:"))
         )
 
-        val msg = InputTextMessageContent("*$queryText*")
-                .parseMode(ParseMode.Markdown)
-                .disableWebPagePreview(true)
+        logger.info("Inline query ${query.id()} from ${query.from().id()}: $queryText")
 
-        val result = InlineQueryResultArticle(pollId, "Start new poll", msg)
-                .description(queryText)
-                .replyMarkup(makeInlineKeyboard(pollId, variants))
-        val resp = AnswerInlineQuery(query.id(), result)
+        val resultArticles = resultArticlesContent.map { articleContent ->
+            val pollId = UUID.randomUUID().toString()
+
+            val msg = InputTextMessageContent("*$queryText*")
+                    .parseMode(ParseMode.Markdown)
+                    .disableWebPagePreview(true)
+
+            val resultArticle = InlineQueryResultArticle(pollId, EmojiParser.parseToUnicode(articleContent.title), msg)
+                    .description(queryText)
+                    .replyMarkup(makeInlineKeyboard(pollId, articleContent.variants))
+
+            val pollRedis = redisTemplate.boundHashOps<String, String>("openpoll_$pollId")
+            pollRedis.put("topic", queryText)
+            pollRedis.expire(10, TimeUnit.DAYS)
+
+            val pollVariants = redisTemplate.boundListOps("openpoll_${pollId}_variants")
+            pollVariants.rightPushAll(*articleContent.variants)
+            pollVariants.expire(10, TimeUnit.DAYS)
+
+            logger.info("Created poll $pollId for query ${query.id()}")
+
+            return@map resultArticle
+        }.toTypedArray()
+
+        val resp = AnswerInlineQuery(query.id(), *resultArticles)
                 .cacheTime(0)
                 .isPersonal(true)
 
-        val pollRedis = redisTemplate.boundHashOps<String, String>("openpoll_$pollId")
-        pollRedis.put("topic", queryText)
-        pollRedis.expire(10, TimeUnit.DAYS)
-
-        val pollVariants = redisTemplate.boundListOps("openpoll_${pollId}_variants")
-        pollVariants.rightPushAll(*variants)
-        pollVariants.expire(10, TimeUnit.DAYS)
-
-        logger.info("Inline query from ${query.from().id()}, pollId PollID: $pollId: $queryText")
         bot.execute(resp)
     }
+
+    @Suppress("ArrayInDataClass")
+    data class ResultArticleContent(
+            val title: String,
+            val variants: Array<String>
+    )
 }
